2630b61132ccf6a48533d955a1eed05e25af816b
Tamas Csesznak <tcses@cae.com>
2022-12-03 13:04:14 +0100

    Begining of the code

---
+++ src/Felhasznalo.java
@@ -0,0 +1,32 @@
+public class Felhasznalo {
+    private int azonosito;
+    private String email;
+    private String jelszo;
+    //Jogosultsag, de az jon a leszarmazottbol.
+
+
+    public void kijelentkezes(){};
+    public void bejelentkezes(){};
+    public void adatmodositas(String adatkulcs, String ujErtek){
+        
+    }
+    public int getAzonosito() {
+        return azonosito;
+    }
+    public void setAzonosito(int azonosito) {
+        this.azonosito = azonosito;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getJelszo() {
+        return jelszo;
+    }
+    public void setJelszo(String jelszo) {
+        this.jelszo = jelszo;
+    }
+};
+  
