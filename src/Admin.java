753e184efeead48b6db3aedaeb5c4718b0eb397a
Tamas Csesznak <tcses@cae.com>
2022-12-03 18:44:43 +0100

    Admin funkciok alapok.

--- src/Admin.java
+++ src/Admin.java
@@ -1,31 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Admin extends Felhasznalo {
     public Admin(int azonosito, String email, String jelszo) {
         super(azonosito, email, jelszo);
     }
-    public void kolcsonezTorleseById(Integer kolcsonzesId) {
+    public void kolcsonezTorleseById() throws NumberFormatException, IOException {
+        System.out.print("Kolcsonzes id: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int kolcsonzesId=Integer.parseInt(reader.readLine());
+        adatbazisManager.adatbazis.kolcsonzesek.removeIf((Kolcsonzes k)-> {
+            return k.getKolcsonzesId()==kolcsonzesId;
+        });
+        adatbazisManager.adatbazis.save();
     };
-    public void autoTorlese(Auto auto) {
+    public void autoTorlese() throws IOException {
+        System.out.print("Auto rendszama: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String rendszam=reader.readLine();
+        adatbazisManager.adatbazis.autok.removeIf((Auto a)-> {
+            return a.getRendszam().equals(rendszam);
+        });
+        adatbazisManager.adatbazis.save();
     };
-    public void utanfutoTorlese(Utanfuto utanfuto) {
-    };
-    public void ugyfelTorlese(Ugyfel ugyfel) {
+    public void ugyfelTorlese() throws NumberFormatException, IOException {
+        System.out.print("Ugyfel azonositoja: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int ugyfelAzonosito=Integer.parseInt(reader.readLine());
+        adatbazisManager.adatbazis.felhasznalok.removeIf((Felhasznalo f)-> {
+            return f.getAzonosito()==ugyfelAzonosito;
+        });
+        adatbazisManager.adatbazis.save();
     };
     public void kolcsonezKezelese(Kolcsonzes newKolcsonzes) {
+
     };
     public void autoAdatainakKezelese(Auto newAuto) {
+
     };
     public void utanfutoAdatainakKezelese(Utanfuto newUtanfuto) {
+
     };
     public void ugyfelAdatainakKezelese(Ugyfel newUgyfel) {
+
+    };
+    public void utanfutoTorlese(Utanfuto utanfuto) {
     };
 
     @Override
     void Menu() {
         String[] options = {
-            "1 - ",
-            "2 - ",
+            "1 - Kolcsonzes torlese",
+            "2 - Ugyfel torlese",
+            "3 - Auto torlese",
             "0 - Vissza",
         };
         Scanner scanner = new Scanner(System.in);
@@ -35,8 +64,9 @@ public class Admin extends Felhasznalo {
             try {
                 option = scanner.nextInt();
                 switch (option){
-                    case 1 : break;
-                    case 2: break;
+                    case 1 :kolcsonezTorleseById(); break;
+                    case 2: ugyfelTorlese(); break;
+                    case 3: autoTorlese(); break;
                     case 0: break;
                 }
             }
